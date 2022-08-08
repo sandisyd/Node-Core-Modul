0133d1613f498cecf7acb929132c1b96e6cedb2d
Sandi <33063833+sandisyd@users.noreply.github.com>
2022-08-08 19:58:24 +0700

    update: add contact list contact detail contact delete contact

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //mengambil argumen dari cmd
 
 const yargs = require('yargs')
-const { simpanContact } = require('./contacts')
+const { simpanContact, listContact, detailContact, deleteContact } = require('./contacts')
 
 yargs.command({
     command: 'add',
@@ -25,6 +25,50 @@ yargs.command({
     },
     handler(argv) {
         simpanContact(argv.nama, argv.email, argv.noHp)
+    }
+}).demandCommand()
+
+
+//menampilkan daftar semua nama kontak
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan semua nama & no Hp Contact',
+    handler(){
+        listContact()
+    }
+})
+
+//menampilkan detail sebuah contact
+yargs.command({
+    command:'detail',
+    describe: 'Menampilkan detail sebuah contact',
+    builder:{
+        nama: {
+            describe: 'Nama Lengkap',
+            demanOption: true,
+            type: 'string'
+        },
+    },
+    handler(argv){
+        detailContact(argv.nama)
+    }
+})
+
+
+//menghapus sebuah contact
+
+yargs.command({
+    command:'delete',
+    describe: 'Menghapus sebuah contact',
+    builder:{
+        nama: {
+            describe: 'Nama Lengkap',
+            demanOption: true,
+            type: 'string'
+        },
+    },
+    handler(argv){
+        deleteContact(argv.nama)
     }
 })
 yargs.parse()
--- contacts.js
+++ contacts.js
@@ -5,36 +5,40 @@ const validator = require('validator')
 
 //reade and create folder
 const dirPath = './data'
-if(!fs.existsSync(dirPath)){
+if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath)
 }
 
 const dataPath = './data/contact.json'
-if(!fs.existsSync(dataPath)){
+if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
-
-const simpanContact = (nama,email,noHp)=>{
-    const contact = {nama, email, noHp}
+const loadContact = () => {
     const file = fs.readFileSync('data/contact.json', 'utf8')
-    const contacts = JSON.parse(file);
+    const contacts = JSON.parse(file)
+    return contacts;
+}
 
+const simpanContact = (nama, email, noHp) => {
+    const contact = { nama, email, noHp }
+
+    const contacts = loadContact()
 
     //cekduplikat
-    const duplikat = contacts.find((contact)=>contact.nama === nama)
-    if(duplikat){
+    const duplikat = contacts.find((contact) => contact.nama === nama)
+    if (duplikat) {
         console.log(chalk.red.bgRed.bold('Contact sudah terdaftar, gunakan nama lain'))
         return false;
     }
 
-    if(email){
-        if(!validator.isEmail(email)){
+    if (email) {
+        if (!validator.isEmail(email)) {
             console.log(chalk.red.inverse.bold('Email tidak valid'))
             return false;
         }
     }
-    if(!validator.isMobilePhone(noHp, 'id-ID')){
+    if (!validator.isMobilePhone(noHp, 'id-ID')) {
         console.log(chalk.red.inverse.bold('Nomor Hp tidak valid'))
         return false;
     }
@@ -46,4 +50,46 @@ const simpanContact = (nama,email,noHp)=>{
     console.log(chalk.green.inverse.bold('Terimakasih sudah memasukkan data'));
 }
 
-module.exports = { simpanContact}
+const listContact = () => {
+    const contacts = loadContact()
+    console.log(chalk.blue.inverse.bold('Data contact : '));
+    contacts.forEach((contact, i) => {
+
+        console.log(`${i + 1}. ${contact.nama} - ${contact.noHp}`)
+    });
+}
+
+
+
+//menampilkan detail dari contact
+const detailContact = (nama) => {
+    const contacts = loadContact()
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase())
+
+    if (!contact) {
+        console.log(chalk.red.inverse.bold(` ${nama} tidak ditemukan!!`))
+        return false;
+    }
+    console.log(chalk.blue.inverse.bold(contact.nama));
+    console.log(chalk.blue.inverse.bold(contact.noHp));
+    if (contact.email) {
+
+        console.log(chalk.blue.inverse.bold(contact.email));
+    }
+
+
+}
+
+const deleteContact = (nama) => {
+    const contacts = loadContact()
+    const newContact = contacts.filter((contact)=> contact.nama.toLowerCase() !== nama.toLowerCase())
+    if (contacts.length === newContact.length) {
+        console.log(chalk.red.inverse.bold(` ${nama} tidak ditemukan!!`))
+        return false;
+    }
+    fs.writeFileSync('data/contact.json', JSON.stringify(newContact));
+    console.log(contacts);
+    console.log(chalk.green.inverse.bold(`data contact ${nama} berhasil dihapus!`));
+
+}
+module.exports = { simpanContact, listContact, detailContact, deleteContact }
